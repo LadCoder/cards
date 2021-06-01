ed37b6fd612d43696674839efa2fee2f0c234760
LadCoder <hbk.lekkas@gmail.com>
2021-06-01 22:32:03 +1000

    Setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // Where files should be sent once they are bundled
- output: {
-   path: path.join(__dirname, '/dist'),
-   filename: 'index.bundle.js'
- },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js'
+  },
   // webpack 5 comes with devServer which loads in development mode
- devServer: {
-   port: 3000,
-   watchContentBase: true
- },
+  devServer: {
+    port: 3000,
+    watchContentBase: true
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  
   // Rules of how webpack will take our files, complie & bundle them for the browser 
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx)$/,
-       exclude: /nodeModules/,
-       use: {
-         loader: 'babel-loader'
-       }
-     },
-     {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader']
-     }
-   ]
- },
- plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 }
